5d23d4cc5fe7155e3f2d285200e308c890bdc5e9
Gabrielle Verissimo <gabrielleverissimo02@gmail.com>
2025-09-10 16:07:51 -0300

    email and password validation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,23 @@
 import { PrismaClient } from '@prisma/client';
 import { UserReq } from './user.model';
+import { createUserValidation } from './user.validation';
 
 const prisma = new PrismaClient();
 
 export async function create(user: UserReq) {
+  const [userEmail, userPassword] = createUserValidation(user);
+  if (!userEmail?.success) {
+    return userEmail?.error.message;
+  }
+  if (!userPassword?.success) {
+    return userPassword?.error.message;
+  }
+  const existsEmail = await uniqueEmail(user.email);
+
+  if (existsEmail) {
+    return 'Já existe um usuario com esse email.';
+  }
+
   const post = await prisma.user.create({
     data: {
       name: user.name,
@@ -14,4 +28,19 @@ export async function create(user: UserReq) {
   });
 
   return post;
+}
+
+async function uniqueEmail(email: string) {
+  try {
+    const uniqueEmail = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    return uniqueEmail;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
