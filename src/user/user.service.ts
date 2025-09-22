9d190533d5d96783be40fe4b6504c9790786dadd
Gabrielle Verissimo <gabrielleverissimo02@gmail.com>
2025-09-22 17:24:44 -0300

    Added setErrorHandler and tests for error outputs

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -9,59 +9,28 @@ const prisma = prismaInstance;
 export async function create(user: UserReq) {
   const [userEmail, userPassword] = createUserValidation(user);
   if (!userEmail?.success) {
-    const errorEmail: string = userEmail?.error.message || '';
-    const error = new UserError(
-      400,
-      errorEmail,
-      'USR_02',
-      'Invalid email. Please check if you entered the email correctly.',
-    );
-    if (error instanceof UserError) {
-      throw {
-        statusCode: error.statusCode,
-        code: error.code,
-        error: error.name,
-        message: error.message,
-        details: error.details,
-      };
-    }
+    const errorEmail: string = userEmail?.error.issues.map(e => e.message).join(' ') || '';
+    throw new UserError(400, errorEmail, 'USR_02', 'Invalid email. Please check if you entered the email correctly.');
   }
   if (!userPassword?.success) {
-    const errorPassword: string = userPassword?.error.message || '';
-    const error = new UserError(
+    const errorPassword: string = userPassword?.error.issues.map(e => e.message).join(' ') || '';
+
+    throw new UserError(
       400,
       errorPassword,
       'USR_01',
       'The password must contain at least 6 characters and among them there must be at least 1 letter and 1 digit.',
     );
-    if (error instanceof UserError) {
-      throw {
-        statusCode: error.statusCode,
-        code: error.code,
-        error: error.name,
-        message: error.message,
-        details: error.details,
-      };
-    }
   }
   const existsEmail = await findByEmail(user.email);
 
   if (existsEmail) {
-    const error = new UserError(
+    throw new UserError(
       400,
       'Invalid credential',
       'USR_03',
       'There is already a user registered with this email. Please provide a different email address.',
     );
-    if (error instanceof UserError) {
-      throw {
-        statusCode: error.statusCode,
-        code: error.code,
-        error: error.name,
-        message: error.message,
-        details: error.details,
-      };
-    }
   }
   user.password = await hashPassword(user.password);
   const post = await prisma.user.create({
--- src/user/user.validation.ts
+++ src/user/user.validation.ts
@@ -4,13 +4,12 @@ import { UserReq } from './user.model';
 const email = z.email();
 const password = z
   .string()
-  .min(6, { message: 'A senha deve ter no mínimo 6 caracteres.' })
-  .regex(/[a-zA-Z]/, { message: 'A senha deve conter pelo menos uma letra.' })
-  .regex(/[0-9]/, { message: 'A senha deve conter pelo menos um dígito.' });
+  .min(6, { message: 'Password must be at least 6 characters long.' })
+  .regex(/[a-zA-Z]/, { message: 'Password must contain at least one letter.' })
+  .regex(/[0-9]/, { message: 'Password must contain at least one digit.' });
 
 export function createUserValidation(user: UserReq) {
   const userEmail = email.safeParse(user.email);
   const userPassword = password.safeParse(user.password);
-
   return [userEmail, userPassword];
 }
